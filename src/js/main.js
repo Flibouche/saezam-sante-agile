75e5fe5212e5601a5c6b0453128d894cc2ba246e
Kevin Pfiffer <kevin.pfiffer2@gmail.com>
2025-03-23 12:42:04 +0100

    📦 NEW: add footer section with logo, menu, and contact information, add script to get the current year to the copyright.

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     const mobileNavToggle = document.getElementById('mobile-nav-toggle');
     const mobileNavMenu = document.getElementById('mobile-nav-menu');
-    const navClose = document.getElementById('mobile-nav-close');
+    const mobileNavClose = document.getElementById('mobile-nav-close');
 
     mobileNavToggle.addEventListener('click', () => {
         mobileNavMenu.classList.remove('translate-x-full');
     });
 
-    navClose.addEventListener('click', () => {
+    mobileNavClose.addEventListener('click', () => {
         mobileNavMenu.classList.add('translate-x-full');
     });
+});
+
+// Copyright Year
+document.addEventListener('DOMContentLoaded', function () {
+    const copyrightYear = document.getElementById('year');
+    const currentYear = new Date().getFullYear();
+    copyrightYear.innerHTML = currentYear;
 });
